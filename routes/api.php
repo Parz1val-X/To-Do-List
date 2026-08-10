9d36a8d061e2e1f68a3d7ad2ad1f5bbe7e876976
Parz1val-X <robertolaime61@gmail.com>
2026-08-09 21:54:02 -0400

    Create api resource controllers and register api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TagController;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('tags', TagController::class);
+Route::apiResource('tasks', TaskController::class);
